7e7dd1e1ea82bdb33f9b39f69998b3004b02c0a5
Tanveer Singh <tsbajaj28@gmail.com>
2023-12-03 22:59:14 +0530

    test2

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
-public class Main{
-	public static void main(String... sss){
+import java.util.Date;
+
+public class Main {
+	public static void main(String... sss) {
 		System.out.println("test success");
+		System.out.println("Date:: " + new Date());
 	}
 }
--- Main2.java
+++
@@ -1,5 +0,0 @@
-public class Main2{
-	public static void main(String... sss){
-		System.out.println("test success main 2");
-	}
-}
