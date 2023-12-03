382a3c3d9c75b66075a73aec888db023dfe5c042
Tanveer Singh <tsbajaj28@gmail.com>
2023-12-03 23:10:28 +0530

    added one more commit

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String... sss) {
 		System.out.println("test success");
 		System.out.println("Date:: " + new Date());
+		System.out.println("added one more print");
 	}
 }
